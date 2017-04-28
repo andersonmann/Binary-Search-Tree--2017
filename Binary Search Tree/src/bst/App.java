84c36116abb0b8757df461410a471eecf1739e04
anderson <anderson.civil@hotmail.com>
2017-04-28 18:36:36 -0300

    primeiro commit

---
+++ Binary Search Tree/src/bst/App.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package bst;
+
+/**
+ * @author andersonm
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Word");
+
+	}
+
+}
---
+++ Binary Search Tree/src/bst/BinarySearchTree.java
@@ -0,0 +1,6 @@
+package bst;
+
+public class BinarySearchTree {
+	
+
+}
